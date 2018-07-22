7dd4a956530ace5db99fef05440e63e5262174ec
Ryan Edward <rj.coight@gmail.com>
2018-07-22 12:44:46 +1000

    basic functionality

--- gui.py
+++ gui.py
@@ -53,7 +53,7 @@ def _create_q_and_a_frame(mm_window: tkinter.Tk) -> tkinter.Frame:
 
 
 def _connect_home_and_q_and_a_frames(home_frame: tkinter.Frame, q_and_a_frame: tkinter.Frame) -> None:
-	selected_q_types: typing.List[tkinter.StringVar] = []
+	selected_q_types: typing.List[str] = []
 	q_type_indexed_q_and_a_generators: typing.Dict[str, MM_QAndAGenerators.Q_AND_A_GENERATOR_SIGNATURE] = MM_QAndAGenerators.get_q_type_indexed_generators()
 
 	cur_home_frame_checkbutton_row: int = 0
@@ -65,13 +65,12 @@ def _connect_home_and_q_and_a_frames(home_frame: tkinter.Frame, q_and_a_frame: t
 		home_frame.grid_rowconfigure(cur_home_frame_checkbutton_row, weight=1)
 		home_frame.grid_columnconfigure(cur_home_frame_checkbutton_col, weight=1)
 		
-		selected_q_types.append(tkinter.StringVar())
+		q_type_checkbutton_var: tkinter.BooleanVar = tkinter.BooleanVar()
 		q_type_checkbutton: tkinter.Checkbutton = tkinter.Checkbutton(
 																master=home_frame, 
 																text=q_type,
-																variable=selected_q_types[-1],
-																onvalue=q_type,
-																offvalue=None
+																variable=q_type_checkbutton_var,
+																command=lambda: selected_q_types.append(q_type) if q_type_checkbutton_var.get() else selected_q_types.remove(q_type)
 																)
 		q_type_checkbutton.grid(
 							row=cur_home_frame_checkbutton_row, 
@@ -99,30 +98,29 @@ def _activate_frame(frame: tkinter.Frame) -> None:
 def _show_q_or_a(
 			a_textbox: tkinter.Text, 
 			q_textbox: tkinter.Text, 
-			selected_q_types: typing.List[tkinter.StringVar],
+			selected_q_types: typing.List[str],
 			q_type_indexed_q_and_a_generators: typing.Dict[str, MM_QAndAGenerators.Q_AND_A_GENERATOR_SIGNATURE]
 			) -> None:
-	print("shi")
-	if not _show_q_or_a.current_q:
+	if "current_q" not in _show_q_or_a.__dict__:
 		(_show_q_or_a.current_q, _show_q_or_a.current_a) = _generate_q_and_a(selected_q_types, q_type_indexed_q_and_a_generators)
 		_show_q_or_a.a_is_shown = False	
 
-	if not _show_q_and_a.a_is_shown:
+	if not _show_q_or_a.a_is_shown:
 		a_textbox.config(state="normal")	
-		a_textbox.delete("start", "end")
-		a_textbox.insert(str(_show_q_and_a.current_a))
+		a_textbox.delete("1.0", "end")
+		a_textbox.insert("1.0", str(_show_q_or_a.current_a))
 		a_textbox.config(state="disabled")	
-		_show_q_and_a.a_is_shown = True
+		_show_q_or_a.a_is_shown = True
 	else:
 		(_show_q_or_a.current_q, _show_q_or_a.current_a) = _generate_q_and_a(selected_q_types, q_type_indexed_q_and_a_generators)
 
 		q_textbox.config(state="normal")	
-		q_textbox.delete("start", "end")
-		q_textbox.insert(_show_q_and_a.current_q)
+		q_textbox.delete("1.0", "end")
+		q_textbox.insert("1.0", _show_q_or_a.current_q)
 		q_textbox.config(state="disabled")	
 
 		a_textbox.config(state="normal")	
-		a_textbox.delete("start", "end")
+		a_textbox.delete("1.0", "end")
 		a_textbox.config(state="disabled")	
 		
 		_show_q_or_a.a_is_shown = False
