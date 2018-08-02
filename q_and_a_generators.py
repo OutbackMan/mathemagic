e092135f3db2c0d00706eebaa4702a55cb7bef47
Russell Coight <rj.coight@gmail.com>
2018-08-03 06:43:48 +1000

    more q and a

--- q_and_a_generators.py
+++ q_and_a_generators.py
@@ -10,11 +10,38 @@ def _ending_in_5_square_2_digit() -> Q_AND_A_SIGNATURE:
 	answer: float = float(eval(question))
 	return (question, answer)
 
+def _multiplication_same_first_add_to_10_2_digit() -> Q_AND_A_SIGNATURE:
+    possible_questions: typing.List = range(10, 100)
+    first_number: int = random.choice(possible_questions)
+    second_number: str = f"{first_number // 10}{10 - (first_number % 10)}"
+    question: str = f"{first_number} * {second_number}"
+    answer: float = float(eval(question))
+    return (question, answer)
+
+def _multiplication_2_digit_with_11() -> Q_AND_A_SIGNATURE:
+    possible_questions: typing.List = range(10, 100)
+    question: str = f"{random.choice(possible_questions)} * 11"
+    answer: float = float(eval(question))
+    return (question, answer)
+
 def _addition_2_digit() -> Q_AND_A_SIGNATURE:
-	possible_questions: typing.List = range(10, 100)
-	question: str = f"{random.choice(possible_questions)} + {random.choice(possible_questions)}"
-	answer: float = float(eval(question))
-	return (question, answer)
+    possible_questions: typing.List = range(10, 100)
+    question: str = f"{random.choice(possible_questions)} + {random.choice(possible_questions)}"
+    answer: float = float(eval(question))
+    return (question, answer)
+
+def _addition_3_digit() -> Q_AND_A_SIGNATURE:
+    possible_questions: typing.List = range(100, 1000)
+    question: str = f"{random.choice(possible_questions)} + {random.choice(possible_questions)}"
+    answer: float = float(eval(question))
+    return (question, answer)
+
+def _addition_4_digit() -> Q_AND_A_SIGNATURE:
+    possible_first: typing.List = range(1000, 10000)
+    possible_second: typing.List = range(10, 1000)
+    question: str = f"{random.choice(possible_first)} + {random.choice(possible_second)}"
+    answer: float = float(eval(question))
+    return (question, answer)
 
 def get_q_type_indexed_generators() -> typing.Dict[str, Q_AND_A_GENERATOR_SIGNATURE]:
 	q_type_indexed_generators: typing.Dict[str, Q_AND_A_GENERATOR_SIGNATURE] = {}
