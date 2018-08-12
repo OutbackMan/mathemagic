6e9b30bd08841b33e01f128870418a6fd24612ef
Russell Coight <rj.coight@gmail.com>
2018-08-13 06:40:14 +1000

    subtraction

--- q_and_a_generators.py
+++ q_and_a_generators.py
@@ -43,6 +43,30 @@ def _addition_4_digit() -> Q_AND_A_SIGNATURE:
     answer: float = float(eval(question))
     return (question, answer)
 
+def _subtraction_2_digit() -> Q_AND_A_SIGNATURE:
+    possible_first: typing.List = range(10, 100)
+    first: int = random.choice(possible_first)
+    possible_second: typing.List = range(first, 9, -1)
+    question: str = f"{first} - {random.choice(possible_second)}"
+    answer: float = float(eval(question))
+    return (question, answer)
+
+def _subtraction_3_digit() -> Q_AND_A_SIGNATURE:
+    possible_first: typing.List = range(100, 1000)
+    first: int = random.choice(possible_first)
+    possible_second: typing.List = range(first, 99, -1)
+    question: str = f"{first} - {random.choice(possible_second)}"
+    answer: float = float(eval(question))
+    return (question, answer)
+
+def _subtraction_4_digit() -> Q_AND_A_SIGNATURE:
+    possible_first: typing.List = range(1000, 10000)
+    first: int = random.choice(possible_first)
+    possible_second: typing.List = range(100, 1000)
+    question: str = f"{first} - {random.choice(possible_second)}"
+    answer: float = float(eval(question))
+    return (question, answer)
+
 def get_q_type_indexed_generators() -> typing.Dict[str, Q_AND_A_GENERATOR_SIGNATURE]:
 	q_type_indexed_generators: typing.Dict[str, Q_AND_A_GENERATOR_SIGNATURE] = {}
 	for (identifier, value) in globals().items():
