b63407b2c9d38c588bf2e220d94a976a4d911529
Russell Coight <rj.coight@gmail.com>
2018-08-15 08:14:20 +1000

    division

--- q_and_a_generators.py
+++ q_and_a_generators.py
@@ -88,6 +88,27 @@ def _square_2_digit() -> Q_AND_A_SIGNATURE:
     answer: float = float(eval(question))
     return (question, answer)
 
+def _multiplication_2_digit() -> Q_AND_A_SIGNATURE:
+    possible_first: typing.List = range(10, 100)
+    possible_second: typing.List = range(10, 100)
+    question: str = f"{random.choice(possible_first)} * {random.choice(possible_second)}"
+    answer: float = float(eval(question))
+    return (question, answer)
+
+def _square_3_digit() -> Q_AND_A_SIGNATURE:
+    possible_first: typing.List = range(100, 1000)
+    possible_second: typing.List = range(100, 1000)
+    question: str = f"{random.choice(possible_first)} * {random.choice(possible_second)}"
+    answer: float = float(eval(question))
+    return (question, answer)
+
+def _division_2_digit() -> Q_AND_A_SIGNATURE:
+    possible_first: typing.List = range(10, 1000)
+    possible_second: typing.List = range(1, 10)
+    question: str = f"{random.choice(possible_first)} / {random.choice(possible_second)}"
+    answer: float = float(eval(question))
+    return (question, answer)
+
 def get_q_type_indexed_generators() -> typing.Dict[str, Q_AND_A_GENERATOR_SIGNATURE]:
 	q_type_indexed_generators: typing.Dict[str, Q_AND_A_GENERATOR_SIGNATURE] = {}
 	for (identifier, value) in globals().items():
